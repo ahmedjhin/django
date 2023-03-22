4bd13bc6f408a791b5a9c56341c52197cbc7da13
ahmedjhin <a7madti90@hotmail.com>
2023-03-22 20:18:02 +0400

    sven

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class room(models.Model):
+    # host = 
+    # topic =
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    # particpants = 
+    updateD = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    kosalghba = models.TextField(auto_created=True)
+    
+    def __str__(self):
+        return self.name
